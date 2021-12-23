19f27f8fabf2fa0e6fd220395ea2d58d33f96800
Ishit Beswal <ib370@snu.edu.in>
2021-12-23 15:59:12 +0530

    initiated app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [HC1cards, setHC1cards] = useState([]);
+    let [HC3cards, setHC3cards] = useState([]);
+    let [HC5cards, setHC5cards] = useState([]);
+    let [HC6cards, setHC6cards] = useState([]);
+    let [HC9cards, setHC9cards] = useState([]);
+
+    let card_design_type = {
+        HC1: setHC1cards,
+        HC3: setHC3cards,
+        HC5: setHC5cards,
+        HC6: setHC6cards,
+        HC9: setHC9cards,
+    };
+
+    /**
+     *
+     * @param {Array} cards
+     */
+    const sortCards = (cards) => {
+        cards.forEach((card) => {
+            let setCards = card_design_type[card.design_type];
+            setCards((curr) => [...curr, card]);
+        });
+    };
+
+    const fetchData = async () => {
+        fetch(process.env.REACT_APP_API_URL)
+            .then(async (res) => {
+                let cards = await res.json();
+                sortCards(cards.card_groups);
+            })
+            .catch((e) => console.log(e));
+    };
+
+    useEffect(() => {
+        fetchData();
+
+        // eslint-disable-next-line
+    }, []);
+
+    return <div className='App'></div>;
 }
 
 export default App;
